b41d9e8b74a730613256a6ce46d1c9d8dbd5c577
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2021-10-08 16:30:51 -0500

    Finished CV design. Fully Translated resume. Added PRINT ALL button

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ const page2 = document.getElementById('page2')
 
 const printElement = (el) => {
     let pageBackup = document.body.innerHTML
-    let printContent = document. getElementById(el).innerHTML
+    let printContent = document.getElementById(el).innerHTML
+    document.body.innerHTML = printContent
+    window.print()
+    document.body.innerHTML = pageBackup
+}
+
+const printAll = () => {
+    let pageBackup = document.body.innerHTML
+    let printContent = document.getElementById('page1').innerHTML + document.getElementById('page2').innerHTML
     document.body.innerHTML = printContent
     window.print()
     document.body.innerHTML = pageBackup

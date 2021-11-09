95a904b7680e9e5bfcc8bc6ad43b1bb3e517c7a5
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2021-11-08 19:12:40 -0500

    Added option to add and remove proyects from protfolio

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const page1 = document.getElementById('page1')
 const page2 = document.getElementById('page2')
 const chk_skill = document.getElementsByClassName('chk_skill')
 const chk_job = document.getElementsByClassName('chk_job')
+const chk_proyect = document.getElementsByClassName('chk_proyect')
 const chk_label = document.getElementById('chk_label')
 const body = document.getElementById('body')
 const selectAllChk = document.getElementById('selectAllChk')
 const deleteAllChk = document.getElementById('deleteAllChk')
 const selectAllChkJob = document.getElementById('selectAllChkJob')
 const deleteAllChkJob = document.getElementById('deleteAllChkJob')
+const selectAllChkProyect = document.getElementById('selectAllChkProyect')
+const deleteAllChkProyect = document.getElementById('deleteAllChkProyect')
 
 const printElement = (el) => {
     let pageBackup = document.body.innerHTML
@@ -52,6 +55,16 @@ const chk_addListeners = () => {
             }
         })
     }
+    for (let index = 0; index < chk_proyect.length; index++) {
+        const proyect = chk_proyect[index];
+        proyect.addEventListener('change', ()=>{
+            if (!proyect.checked) {
+                document.getElementById(`div_${proyect.id}`).style.display = 'none'
+            } else {
+                document.getElementById(`div_${proyect.id}`).style.display = 'flex'
+            }
+        })
+    }
 }
 
 const chk_initialValue = () => {
@@ -71,6 +84,14 @@ const chk_initialValue = () => {
             job.checked = true
         }
     }
+    for (let index = 0; index < chk_proyect.length; index++) {
+        const proyect = chk_proyect[index];
+        if (document.getElementById(`div_${proyect.id}`).style.display == 'none') {
+            proyect.checked = false
+        } else {
+            proyect.checked = true
+        }
+    }
 }
 
 const chk_initialValue_inverse = () => {
@@ -88,6 +109,14 @@ const chk_initialValue_inverse = () => {
             document.getElementById(`div_${job.id}`).style.display = 'none'
         } else {
             document.getElementById(`div_${job.id}`).style.display = 'flex'
+        }
+    }
+    for (let index = 0; index < chk_proyect.length; index++) {
+        const proyect = chk_proyect[index];
+        if (!proyect.checked) {
+            document.getElementById(`div_${proyect.id}`).style.display = 'none'
+        } else {
+            document.getElementById(`div_${proyect.id}`).style.display = 'flex'
         }
     }
 }
@@ -133,6 +162,22 @@ deleteAllChkJob.addEventListener('click', () => {
     for (let index = 0; index < chk_job.length; index++) {
         const job = chk_job[index];
         job.checked = false
+    }
+    chk_initialValue_inverse()
+})
+
+selectAllChkProyect.addEventListener('click', () => {
+    for (let index = 0; index < chk_proyect.length; index++) {
+        const proyect = chk_proyect[index];
+        proyect.checked = true
+    }
+    chk_initialValue_inverse()
+})
+
+deleteAllChkProyect.addEventListener('click', () => {
+    for (let index = 0; index < chk_proyect.length; index++) {
+        const proyect = chk_proyect[index];
+        proyect.checked = false
     }
     chk_initialValue_inverse()
 })
